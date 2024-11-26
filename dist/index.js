3806184546e6cec9e5d612e05c98f28d68e2b1b0
rsarker1 <rahul.sarker@stonybrook.edu>
2024-11-26 16:27:53 -0500

    Bot is online

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log('Test');
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv = require("dotenv");
+const discord_js_1 = require("discord.js");
+dotenv.config();
+const client = new discord_js_1.Client({
+    intents: [discord_js_1.GatewayIntentBits.Guilds]
+});
+client.on('ready', b => {
+    console.log(`${b.user.tag} is online.`);
+});
+client.login(process.env.TOKEN);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import 'dotenv/config'
 import { Client, GatewayIntentBits } from 'discord.js'
 
+
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds]
+  intents: [GatewayIntentBits.Guilds],
 })
+
+client.on('ready', (b) => {
+  console.log(`${b.user.displayName} is online.`)
+})
+
+client.login(process.env.TOKEN)
